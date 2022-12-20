40a777d8c9cb1f5a667d3b147b486b00ce52f4f5
ShivaPD <shivakumardhulehole@gmail.com>
2022-12-20 11:20:43 +0530

    Update Today

---
+++ src/test/java/Punter1.java
@@ -0,0 +1,18 @@
+
+public class Punter1 {
+
+public int hashCode()
+		{
+			return 143;
+		}
+		
+		public static void main(String[] args) 
+		{
+			Punter1 p=new Punter1();
+			System.out.println(p.hashCode());
+		}
+
+
+	
+
+}

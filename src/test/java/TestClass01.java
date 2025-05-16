8bf414e675e893f6af2fa2641dd782fa5b6d6f0f
mahesh9740 <mahesh.931@outlook.com>
2025-05-17 02:03:05 +0530

    Removed 2 lines

--- src/test/java/TestClass01.java
+++ src/test/java/TestClass01.java
@@ -4,10 +4,6 @@ public class TestClass01 {
 	public static void main(String[] args) {
 		System.out.println("Hello Java!");
 
-
-		System.out.println("Hello Java1!");
-		System.out.println("Hello Java2!");
-
 	}
 
 }

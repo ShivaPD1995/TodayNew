71b729c1f6c5a16e2099f26f7dfa88e17ec8bbc0
mahesh9740 <mahesh.931@outlook.com>
2025-05-17 01:40:17 +0530

    Second Commit

--- src/test/java/TestClass01.java
+++ src/test/java/TestClass01.java
@@ -4,6 +4,7 @@ public class TestClass01 {
 	public static void main(String[] args) {
 		System.out.println("Hello Java!");
 		System.out.println("Hello Java1!");
+		System.out.println("Hello Java2!");
 
 	}
 
